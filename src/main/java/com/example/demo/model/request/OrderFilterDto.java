3d69ee5294ec54f725dd44bdb5096bb94abb4d1f
buitrongkim214 <buitrongkim214@gmail.com>
2023-11-07 17:55:21 +0900

    1107: - fix lại OrderFilterDto

--- src/main/java/com/example/demo/model/request/OrderFilterDto.java
+++ src/main/java/com/example/demo/model/request/OrderFilterDto.java
@@ -2,6 +2,7 @@ package com.example.demo.model.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 
 @AllArgsConstructor
 @Data
@@ -11,4 +12,7 @@ public class OrderFilterDto {
     private String phone;
     private Integer status;
     private Integer page;
+    public OrderFilterDto() {
+        this.page = 0; // Gán giá trị mặc định cho page là 0
+    }
 }
